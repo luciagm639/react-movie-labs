d789be30bba0098e78ea1710733ce849a6b7bebe
Lucia <luciagm639@gmail.com>
2023-10-20 17:11:44 +0200

    Added Upcoming movies page.

--- movies/src/index.js
+++ movies/src/index.js
@@ -6,6 +6,7 @@ import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+        <Route path="/movies/upcoming" element={ <UpcomingMoviesPage /> } />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
